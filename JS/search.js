5dbea803aab96b52f5b4d46e3dad8def9f145e07
Dimitris Dimosiaris <dimitrisdimosiaris@gmail.com>
2022-05-11 05:06:59 +0300

    Delete JS folder

--- JS/search.js
+++
@@ -1,8 +0,0 @@
-var url = window.location.href;
-
-var cut_url = url.split('#')[1];
-
-var parent = document.getElementById(cut_url).parentElement;
-var child = parent.children[1];
-
-child.classList.add("show");
